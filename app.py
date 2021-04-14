c0d443c01bca9bfbc17f45189827021adafc5c46
bbrady <benbradypm@protonmail.com>
2021-04-14 11:45:41 -0400

    Minor updates to the css

--- app.py
+++ app.py
@@ -8,14 +8,13 @@ def hello_world():
     <html lang="en">
       <head>
         <title>Ben Brady</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/darkly/bootstrap.min.css" integrity="sha384-nNK9n28pDUDDgIiIqZ/MiyO3F4/9vsMtReZK39klb/MtkZI3/LtjSjlmyVPS3KdN" crossorigin="anonymous">
+        <link rel="stylesheet" href="static/stylesheets/style.css">
     </head>
     <body>
-        <div style="text-align: center;">
-            <h1>Coming soon!<h1>
-            <br>
+        <div class="centerbanner">
+            <h1>Coming soon!</h1>
             <img src="https://64.media.tumblr.com/c71f985d1d6f5233cdd468d2799143dc/tumblr_owdgo0jdmI1ur5m8bo2_r2_400.gifv" alt="You can always tell a Milford man...">
-            <br>
+            <br></br>
             <a href="/egg">Egg...</a>
         </div>
     </body>
@@ -27,13 +26,14 @@ def show_image():
     <!DOCTYPE html>
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Ben Brady</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/darkly/bootstrap.min.css" integrity="sha384-nNK9n28pDUDDgIiIqZ/MiyO3F4/9vsMtReZK39klb/MtkZI3/LtjSjlmyVPS3KdN" crossorigin="anonymous">
+        <link rel="stylesheet" href="static/stylesheets/style.css">
     </head>
     <body>
-        <div style="text-align: center;">
+        <div class="centerbanner">
             <img src="https://img.buzzfeed.com/buzzfeed-static/static/2017-12/27/8/asset/buzzfeed-prod-fastlane-03/sub-buzz-19959-1514379884-1.jpg?downsize=600:*&output-format=auto&output-quality=auto" alt="egg...">
-            <br>
+            <br></br>
             <a href="/">Back</a>
         </div>
     </body>
